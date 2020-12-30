bd0372ad6024414084548a6496bf5f03debfcdef
aaronmhr <aaronmhr@gmail.com>
2020-12-31 00:33:47 +0100

    Create a publisher from Notification Center vs traditional observing

--- RWCombine/Combine.playground/Pages/Publishers & Subscribers.xcplaygroundpage/Contents.swift
+++ RWCombine/Combine.playground/Pages/Publishers & Subscribers.xcplaygroundpage/Contents.swift
@@ -1,4 +1,28 @@
 //: [Previous](@previous)
 
+import Foundation
+
+example(of: "Publisher") {
+
+    let myNotification = Notification.Name("MyNotification")
+
+    let publisher = NotificationCenter.default
+        .publisher(for: myNotification, object: nil)
+
+    let center = NotificationCenter.default
+
+    let observer = center.addObserver(
+        forName: myNotification,
+        object: nil,
+        queue: nil) { notification in
+        print("Notification Received!")
+    }
+
+    center.post(name: myNotification, object: nil)
+
+    center.removeObserver(observer)
+}
+
+
 
 //: [Next](@next)
