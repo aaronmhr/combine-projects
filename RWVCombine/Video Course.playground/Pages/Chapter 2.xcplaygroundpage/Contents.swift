a8e921db6eea7b12ac2bce1a12ba9848b273d91c
Aaron Huánuco Ramos <aaron.hr@protonmail.com>
2021-02-06 15:11:17 +0100

    Notification centre example

--- RWVCombine/Video Course.playground/Pages/Chapter 2.xcplaygroundpage/Contents.swift
+++ RWVCombine/Video Course.playground/Pages/Chapter 2.xcplaygroundpage/Contents.swift
@@ -1,3 +1,21 @@
 import Combine
+import Foundation
 
 var subscriptions = Set<AnyCancellable>()
+
+example(of: "NotificationCenter") {
+    let center = NotificationCenter.default
+    let myNotification = Notification.Name("MyNotification")
+
+    let publisher = center
+        .publisher(for: myNotification, object: nil)
+
+    let subscription = publisher
+        .print()
+        .sink { _ in
+            print("Notification received from a publisher!")
+        }
+
+    center.post(name: myNotification, object: nil)
+    subscription.cancel()
+}
