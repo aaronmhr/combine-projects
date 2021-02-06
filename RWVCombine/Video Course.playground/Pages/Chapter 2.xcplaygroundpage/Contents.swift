5fa50c150674e453ffe75912596a514864b19770
Aaron Huánuco Ramos <aaron.hr@protonmail.com>
2021-02-06 16:52:32 +0100

    Example of PassthroughSubject

--- RWVCombine/Video Course.playground/Pages/Chapter 2.xcplaygroundpage/Contents.swift
+++ RWVCombine/Video Course.playground/Pages/Chapter 2.xcplaygroundpage/Contents.swift
@@ -48,3 +48,17 @@ example(of: "assign(to:on:)") {
         .assign(to: \.value, on: object)
         .store(in: &subscriptions)
 }
+
+example(of: "PassthroughSubject") {
+    let subject = PassthroughSubject<String, Never>()
+
+    subject
+        .sink(receiveValue: {print($0) })
+        .store(in: &subscriptions)
+
+    subject.send("Hello")
+    subject.send("World")
+    subject.send(completion: .finished)
+
+    subject.send("Still there?")
+}
