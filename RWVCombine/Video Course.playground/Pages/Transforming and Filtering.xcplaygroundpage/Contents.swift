e4296ba77282a2b1fb708e171cf3b199e8cce325
Aaron Huánuco Ramos <aaron.hr@protonmail.com>
2021-02-06 17:37:40 +0100

    Example of collect

--- RWVCombine/Video Course.playground/Pages/Transforming and Filtering.xcplaygroundpage/Contents.swift
+++ RWVCombine/Video Course.playground/Pages/Transforming and Filtering.xcplaygroundpage/Contents.swift
@@ -2,3 +2,11 @@ import Combine
 import Foundation
 
 var subscriptions = Set<AnyCancellable>()
+
+example(of: "collect") {
+    ["A", "B", "C", "D", "E"].publisher
+        .collect(2)
+        .sink(receiveCompletion: { print("⚠️", $0) },
+              receiveValue: { print("🔡", $0) })
+        .store(in: &subscriptions)
+}
