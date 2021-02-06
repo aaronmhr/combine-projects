4d8bd0e2892ad1e28bad0eb27c5e8e2579ca3df1
Aaron Huánuco Ramos <aaron.hr@protonmail.com>
2021-02-07 00:35:32 +0100

    Example of switchToLatest

--- RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
+++ RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
@@ -150,3 +150,32 @@ example(of: "Making Phone Numbers") {
         .store(in: &subscriptions)
 
 }
+
+example(of: "switchToLatest") {
+    let publisher1 = PassthroughSubject<Int,Never>()
+    let publisher2 = PassthroughSubject<Int,Never>()
+    let publisher3 = PassthroughSubject<Int,Never>()
+
+    let publishers = PassthroughSubject<PassthroughSubject<Int,Never>,Never>()
+
+    publishers
+        .switchToLatest()
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+
+    publishers.send(publisher1)
+    publisher1.send(1)
+    publisher1.send(2)
+
+    publishers.send(publisher2)
+    publisher1.send(3)
+    publisher2.send(4)
+    publisher2.send(5)
+
+    publishers.send(publisher3)
+    publisher2.send(6)
+    publisher3.send(7)
+    publisher3.send(8)
+    publisher3.send(9)
+}
+
