69695141530027b8cc113d90a272dcb8c697282d
Aaron Huánuco Ramos <aaron.hr@protonmail.com>
2021-02-06 20:39:07 +0100

    Example of last(where:)

--- RWVCombine/Video Course.playground/Pages/Transforming and Filtering.xcplaygroundpage/Contents.swift
+++ RWVCombine/Video Course.playground/Pages/Transforming and Filtering.xcplaygroundpage/Contents.swift
@@ -220,3 +220,16 @@ example(of: "first(where:)") {
         )
         .store(in: &subscriptions)
 }
+
+example(of: "last(where:)") {
+    let numbers = (1...9).publisher
+
+    numbers
+        .print()
+        .last(where: { $0 % 2 == 0} )
+        .sink(
+            receiveCompletion: { print("Completed with: ", $0) },
+            receiveValue: { print("✅", $0) }
+        )
+        .store(in: &subscriptions)
+}
