a376de8a25caacd3646695caf88a6abe9f583e47
Aaron Huánuco Ramos <aaron.hr@protonmail.com>
2021-02-06 23:35:54 +0100

    Example of prepend(output) - in depth

--- RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
+++ RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
@@ -12,3 +12,31 @@ example(of: "prepend(output)") {
         .sink(receiveValue: { print($0) })
         .store(in: &subscriptions)
 }
+
+example(of: "prepend(output) - in depth") {
+    let p0 = PassthroughSubject<Int, Never>()
+    let p2 = PassthroughSubject<Int, Never>()
+    let p3 = PassthroughSubject<Int, Never>()
+
+    p0
+        .prepend(100, 200)
+        .prepend(p2)
+        .prepend(p3)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+
+    p0.send(0)
+    p0.send(1)
+
+    p2.send(10)
+    p2.send(11)
+
+    p3.send(20)
+    p3.send(21)
+    p3.send(completion: .finished)
+
+    p2.send(12) // it doesnt begin to emit any value of p2 until p3 has finished
+    p2.send(completion: .finished) // once p2 has finished it can begin to emit the values of prepend with variadic elements because it has definite size
+
+    p0.send(2) // only then it can begin to emit p0 values
+}
