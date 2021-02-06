3bc4a1a33b81ca4a12c5ee621a0e12d5295fae1f
Aaron Huánuco Ramos <aaron.hr@protonmail.com>
2021-02-06 23:28:11 +0100

    Example of prepend(output)

--- RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
+++ RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
@@ -2,3 +2,13 @@ import Combine
 import Foundation
 
 var subscriptions = Set<AnyCancellable>()
+
+example(of: "prepend(output)") {
+    let publisher = [3,4].publisher
+
+    publisher
+        .prepend(1, 2)
+        .prepend(-1, 0)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
