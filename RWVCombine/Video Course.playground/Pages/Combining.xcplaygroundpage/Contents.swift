71a4e6f87a2f1ba98938705446ae3149f23806e8
Aaron Huánuco Ramos <aaron.hr@protonmail.com>
2021-02-06 23:42:37 +0100

    Example of prepend(Publisher) - passthrough

--- RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
+++ RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
@@ -61,3 +61,17 @@ example(of: "prepend(Publisher)") {
         .sink(receiveValue: { print($0) })
         .store(in: &subscriptions)
 }
+
+example(of: "prepend(Publisher) - passthrough") {
+    let publisher1 = [3,4].publisher
+    let publisher2 = PassthroughSubject<Int, Never>()
+
+    publisher1
+        .prepend(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+
+    publisher2.send(1)
+    publisher2.send(2)
+    publisher2.send(completion: .finished)
+}
