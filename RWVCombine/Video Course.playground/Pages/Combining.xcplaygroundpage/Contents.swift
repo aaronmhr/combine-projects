a33cc088d6779e524d8c457ed5c11162a9863e24
Aaron Huánuco Ramos <aaron.hr@protonmail.com>
2021-02-07 01:31:31 +0100

    Example of Making Phone Numbers Part 2

--- RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
+++ RWVCombine/Video Course.playground/Pages/Combining.xcplaygroundpage/Contents.swift
@@ -259,3 +259,22 @@ example(of: "zip") {
     publisher2.send(completion: .finished)
 
 }
+
+example(of: "Making Phone Numbers Part 2") {
+
+  let phoneNumbersPublisher = ["123-4567", "555-1212", "555-1111", "123-6789"].publisher
+  let areaCodePublisher = ["410", "757", "800", "540"].publisher
+  let phoneExtensionPublisher = ["EXT 901", "EXT 523", "EXT 137", "EXT 100"].publisher
+
+    areaCodePublisher
+        .zip(phoneNumbersPublisher)
+        .map { $0 + "-" + $1 }
+        .zip(phoneExtensionPublisher)
+        .map { $0 + " " + $1 }
+        .sink { print($0) }
+        .store(in: &subscriptions)
+
+
+
+
+}
